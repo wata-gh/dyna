767109451115d1d123a49aeeabb6e5a918b635c7
shinya-watanabe <shinya-watanabe@cookpad.com>
2017-02-02 16:00:09 +0900

    Support DynamoDBLocal

--- lib/dyna/client.rb
+++ lib/dyna/client.rb
@@ -6,7 +6,16 @@ module Dyna
     def initialize(options = {})
       @options = OpenStruct.new(options)
       @options_hash = options
-      @options.ddb = Aws::DynamoDB::Client.new
+      if @options.use_local
+        @options.ddb = Aws::DynamoDB::Client.new(
+          endpoint: @options.dynamo_endpoint || 'http://127.0.0.1:8000',
+          region: @options.region || 'ap-northeast-1',
+          access_key_id: @options.access_key_id || 'dummy',
+          secret_access_key: @options.secret_access_key || 'dummy',
+        )
+      else
+        @options.ddb = Aws::DynamoDB::Client.new
+      end
     end
 
     def apply(file)
